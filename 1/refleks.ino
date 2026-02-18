4f14ac0d6ad411c0f1c4bb88d251e6e7b7c414f0
fablabdioda <fablabdioda@gmail.com>
2026-02-18 12:47:29 +0100

    Update refleks.ino

--- 1/refleks.ino
+++ 1/refleks.ino
@@ -1,8 +1,8 @@
-const int LED = 9;
-const int BUTTON = 14;
+#define LED 9;
+#define BUTTON 4;
 
-unsigned long ledTime;      // moment zapalenia LED
-unsigned long delayTime;    // losowy czas oczekiwania
+unsigned long delayTime;    // losowy czas oczekiwania na zapalenie LED
+unsigned long ledTime;      // czas w któ®ym zapaliła się LED
 bool waiting = false;       // czy LED świeci i czekamy na klik
 
 void setup() {
@@ -10,18 +10,19 @@ void setup() {
   pinMode(BUTTON, INPUT_PULLUP);
   Serial.begin(9600);
   
-  randomSeed(analogRead(A0));  //generujemy źródło losowości
+  randomSeed(analogRead(A0));  //generujemy źródło losowości, odczytując szum z niepodłączonego wejścia analogowego
 
-  delayTime = random(2000, 5000);   // pierwszy losowy czas
+  delayTime = random(2000, 5000);   // generujemy pierwszy losowy czas, z zakresu 2000 do 5000 ms
   Serial.println("Nowa runda...");
+  
 }
 
 void loop() {
 
-  // Zapalenie LED po losowym czasie
-  if (!waiting && millis() > delayTime) {
+  if (!waiting && millis() > delayTime) {   // jeśli nie czekamy na klik i losowy czas już upłynął
+                                            // (funkcja millis() daje milisekundy od startu programu)
     digitalWrite(LED, HIGH);
-    ledTime = millis();
+    ledTime = millis();                     // zapisujemy czas, w którym LED się włączyła
     waiting = true;
   }
 
@@ -36,12 +37,14 @@ void loop() {
     Serial.print(reaction);
     Serial.println(" ms");
 
-    delay(300);  // debounce
+    delay(300);  // debounce przycisku
 
-    // Nowa runda
+    // startujemy nową rundę
     delayTime = millis() + random(2000, 5000);
     waiting = false;
 
     Serial.println("Nowa runda...");
   }
 }
+
+// spróbuj dodać funkcję wykrywającą falstart
